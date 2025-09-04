132fbafadc3accedacc203980562b19b03bbd168
CodeByisaac <highzik862@gmail.com>
2025-09-04 04:26:08 -0400

    init commit

---
+++ src/main/java/com/victor/documenttracker/controller/DocController.java
@@ -0,0 +1,27 @@
+package com.victor.documenttracker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/documents")
+public class DocController {
+    //@Autowired
+
+
+
+
+    /*
+    @PostMapping("/upload")
+    public ResponseEntity<DocumentResponse> uploadDocument(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("category) String category) {
+            // validate pdf file
+            // extract metadata
+            //store file
+            //save to database
+            //return document info
+    }
+    */
+}
