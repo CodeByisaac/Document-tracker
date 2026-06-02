4c63b227ad85a58aa6ca0363232bdbbdc00b9776
CodeByisaac <highzik86@gmail.com>
2026-06-02 18:21:30 -0400

    Add lombok for getter setters

--- src/main/java/com/victor/documenttracker/model/User.java
+++ src/main/java/com/victor/documenttracker/model/User.java
@@ -1,8 +1,10 @@
 package com.victor.documenttracker.model;
 
+import lombok.*;
 import jakarta.persistence.*;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name="users")
 
@@ -36,42 +38,4 @@ public class User {
         this.email = email;
         this.role = role;
     }
-
-    //getters setters
-    public Long getId(){
-        return id;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public Role getRole(){
-        return role;
-    }
-
-    public void setRole(Role role){
-        this.role = role;
-    }
-
 }
