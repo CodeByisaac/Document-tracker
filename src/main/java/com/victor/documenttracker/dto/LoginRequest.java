3b639499b6e8c76161c2afdc1c09dbcfe3843785
CodeByisaac <highzik86@gmail.com>
2026-07-06 17:55:13 -0400

    remove manual getters/setters

--- src/main/java/com/victor/documenttracker/dto/LoginRequest.java
+++ src/main/java/com/victor/documenttracker/dto/LoginRequest.java
@@ -1,26 +1,11 @@
 package com.victor.documenttracker.dto;
 
-import lombok.*;
+import lombok.Data;
 
-//get users credentials
 @Data
 public class LoginRequest {
     private String username;
     private String password;
-
-    public String getUsername(){
-        return username;
-    }
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
 }
 
 
